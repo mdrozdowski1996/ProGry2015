ac793a7edb3c424cbe99d2fbc0316647022b4cff
Maciej Wachulec <makaan94@gmail.com>
2015-11-26 20:08:53 +0100

    Random rooms across the board

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
+#include <cstdlib>
+#include <ctime>
 #include <fstream>
 #include <iostream>
 #include <string>
-
 /*
 @ - hero
 # - wall
@@ -23,6 +24,9 @@ class Level {
 	friend std::istream & operator >>(std::istream &is, Level &level);
 	friend std::ostream & operator <<(std::ostream &os, Level &level);
 public:
+	Level() = default;
+	Level(char *data, int rows, int cols);
+
 	int cols() const;
 	int rows() const;
 	int heroPos() const;
@@ -31,12 +35,18 @@ public:
 
 	bool onlyOneHero() const;
 	bool moveHero(int a, int b);
+
 private:
 	int cols_;
 	int rows_;
 	int heroPos_;
 	char *data_;
 };
+
+Level::Level(char *data, int rows, int cols)
+	: cols_(cols), rows_(rows), data_(data)
+{
+}
 
 int Level::cols() const
 {
@@ -150,10 +160,58 @@ void Player::setPos(int position)
 	pos_ = position;
 }
 
+class MapGenerator {
+public:
+	Level createLevel(int seed = -1)
+	{
+		if (seed == -1)
+			srand(time(nullptr));
+		else
+			srand(seed);
+
+		rows_ = 40;
+		cols_ = 80;
+		canvas_ = new char[rows_ * cols_];
+
+		for(int i = 0; i < rows_; i++)
+			for (int j = 0; j < cols_; j++)
+				canvas_[i * cols_ + j] = Wall;
+
+		int currRow, currCol;
+
+		for (int i = 0; i < 10; i++)
+		{
+			currRow = rand() % (rows_ - 2) + 1;
+			currCol = rand() % (cols_ - 2) + 1;
+			createRectangularRoom(currRow, currCol, rand() % 7 + 3, rand() % 7 + 3);
+		}
+
+		return Level(canvas_, rows_, cols_);
+	}
+
+private:
+	void createRectangularRoom(int posCol, int posRow, int width, int height)
+	{
+		width  = std::min(width, cols_ - posRow - 1);
+		height = std::min(height, rows_ - posCol - 1);
+		posCol = std::max(posCol, 1);
+		posRow = std::max(posRow, 1);
+		for (int i = posCol; i < posCol + height; ++i)
+			for (int j = posRow; j < posRow + width; ++j)
+				canvas_[i * cols_ + j] = Floor;
+	}
+
+	char *canvas_;
+	int rows_;
+	int cols_;
+
+};
+
 int main()
 {
 	Level level;
-	level.readFromFile("lev.txt");
+	MapGenerator gen;
+	level = gen.createLevel();
 
 	Player player;
 	player.setPos(level.cols() + 1);//for now @ has to be top left in lev.txt
